3e9822a1f904fab09c4484519ba94c1fe9fc6a45
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-09-23 10:54:40 +0200

    Commit inicial

---
+++ assets/js/juego.js
@@ -0,0 +1,48 @@
+/**
+ * Vamos a crear dos montones de tarjetas, uno de películas y otro de recursos relacionados:
+ * 
+ */
+const NMOVIES = 5
+const NELEMENTSPMOVIE = 3
+const getMoviesDeck = () => {
+    let movieDeck = []
+    for(let i = 1; i <= NMOVIES; i++) {
+        movieDeck.push("0"+i+"M")
+    }
+    //Barajamos con un método dela librería Underscore. Esta librería ofrece muchas funciones,
+    //en este caso uso shuffle que recibe un arrayy lo devuelve de forma aleatoria
+    movieDeck = _.shuffle(movieDeck)
+    return movieDeck;
+}
+const quitarPeli = (listaPelis) => {
+    if(listaPelis.lenght === 0){
+        throw 'No hay mas tarjetas'
+    }
+    tarjeta = listaPelis.pop()
+    return tarjeta
+}
+const getElementsDeck = () => {
+    let elementDeck = []
+    for(let i = 1; i <= NMOVIES; i++) {
+        for(let j = 1; j <= NELEMENTSPMOVIE; j++) {
+            elementDeck.push("0"+i+"C"+j)
+        } 
+    }
+    //Barajamos
+    elementDeck = _.shuffle(elementDeck)
+    return elementDeck;
+}
+
+let movieDeck = getMoviesDeck()
+let elementDeck = getElementsDeck()
+
+const eventoClick = () => {
+    const botonClick = document.getElementById('btn pelicula')
+    botonClick.addEventListener('click', ()=>{
+        peli = quitarPeli(movieDeck)
+        peli muestras
+    }
+    )
+}
+    //Cuando le demos al click se ponga una carta aleatoria 
+
